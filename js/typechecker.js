b7f89ff9da7416cd68805264f1f62c9e4c5bef26
kim-atkinson <kimfromtoronto@gmail.com>
2023-03-23 19:10:01 -0400

    Added function to change line height.

--- js/typechecker.js
+++ js/typechecker.js
@@ -1,6 +1,9 @@
 const sentenceTag = document.querySelector(`input[type="text"]`)
 const typesizeTag = document.querySelector(`input[name="typesize"]`)
 const typesizeOutput = document.querySelector("span.typesize-output")
+
+const lineheightTag = document.querySelector(`input[name="lineheight"]`)
+const lineheightOutput = document.querySelector("span.lineheight-output")
 const outputTag = document.querySelector("textarea.output")
 const originalText = outputTag.value
 
@@ -25,6 +28,13 @@ outputTag.addEventListener("keyup", function () {
 // When the typesize slider changes, update the text next to it AND
 // Change the outputTag's font size
 typesizeTag.addEventListener("input", function () {
+    // Changes the CSS
     outputTag.style.fontSize = this.value + "px"
+    // Changes the HTML
     typesizeOutput.innerHTML = this.value + "px"
+})
+
+lineheightTag.addEventListener("input", function () {
+    outputTag.style.lineHeight = this.value
+    lineheightOutput.innerHTML = this.value
 })
