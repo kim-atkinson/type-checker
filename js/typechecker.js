560f8451a89881aa1fd107d4cd262ef976a2f7a4
kim-atkinson <kimfromtoronto@gmail.com>
2023-03-23 13:13:42 -0400

    Hooked JS to range tag.

--- js/typechecker.js
+++ js/typechecker.js
@@ -1,6 +1,9 @@
 const sentenceTag = document.querySelector(`input[type="text"]`)
+const typesizeTag = document.querySelector(`input[name="typesize"]`)
+const typesizeOutput = document.querySelector("span.typesize-output")
 const outputTag = document.querySelector("textarea.output")
 const originalText = outputTag.value
+
 
 // When use types the sectence tag, update the output tag accordingly
 // When ther is no value, display orginal text
@@ -17,4 +20,11 @@ sentenceTag.addEventListener("keyup", function () {
 // When type in output tag update the sentence tag accordingly. 
 outputTag.addEventListener("keyup", function () {
     sentenceTag.value = this.value
+})
+
+// When the typesize slider changes, update the text next to it AND
+// Change the outputTag's font size
+typesizeTag.addEventListener("input", function () {
+    outputTag.style.fontSize = this.value + "px"
+    typesizeOutput.innerHTML = this.value + "px"
 })
