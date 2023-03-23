7fc4f4b8eeb8a043d1041cbd06fa893b93c730ad
kim-atkinson <kimfromtoronto@gmail.com>
2023-03-23 10:47:52 -0400

    Added two way typing.

--- js/typechecker.js
+++ js/typechecker.js
@@ -1,7 +1,20 @@
 const sentenceTag = document.querySelector(`input[type="text"]`)
-const outputTag = document.querySelector("section.output")
+const outputTag = document.querySelector("textarea.output")
+const originalText = outputTag.value
 
 // When use types the sectence tag, update the output tag accordingly
+// When ther is no value, display orginal text
 sentenceTag.addEventListener("keyup", function () {
-    outputTag.innerHTML = this.value
+    if (this.value) {
+        outputTag.value = this.value
+    } else {
+        outputTag.value = originalText
+    }
+    
+    // outputTag.innerHTML = this.value
+})
+
+// When type in output tag update the sentence tag accordingly. 
+outputTag.addEventListener("keyup", function () {
+    sentenceTag.value = this.value
 })
